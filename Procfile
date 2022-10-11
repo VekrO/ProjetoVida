af5f46bbefffd245baf5b50de8b5167c70cd4877
Renan Godoy Silva <cgrenancontato@gmail.com>
2022-10-11 00:39:04 -0400

    Primeiro

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn projeto_vida.wsgi --log-file -
--- projetovida/settings.py
+++ projetovida/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-=nte@bdzxu^)44aj)m!+))!vmgw*7!^(b8xigt*1_$c2dbxx@m'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
